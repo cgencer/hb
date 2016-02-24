4aed88c9055bc518b1e39066ec58916811d6b914
Cem Gencer <cg@artBees-cg.local>
2016-02-24 18:02:33 +0200

    grunt ok for now.

--- gulpfile.js
+++ gulpfile.js
@@ -21,42 +21,60 @@
 		bower: './bower.json',
 		copyright: './copyright.txt',
 		version: './version.txt',
-		toClean: ['dist/js/*.js', 'dist/*.html'],
+		vendor: 'dist/js/vendor',
+		toClean: ['dist/css/*', 'dist/js/*.js', 'dist/*.html'],
 		jsSrc: ['src/js/*.js'],
 		jsDst: ['dist/js/*.js'],
 		cssSrc: 'src/styles/*.css',
 		cssDstPath: 'dist/css',
+		jsSrcPath: 'src/js',
 		jsDstPath: 'dist/js',
-		htmlSrc: 'src/*.html',
+		htmlSrc: ['src/*.html'],
 		htmlDstPath: 'dist',
+		jsSrcCombined: 'dest/js/combined.js',
+		jsDstCombined: 'dest/js/combined-min.js',
 	};
+
+	gulp.task('wiredep',function(){
+
+		gulp.src(files.htmlSrc)
+			.pipe( wiredep({
+				directory: files.vendor,
+				bowerJson: require(files.bower),
+			}) )
+			.pipe( useref() )
+			.pipe( gulpif('*.css', minCss()) )
+			.pipe( gulp.dest(files.htmlDstPath) );
+	});
 
 	gulp.task('default', function() {
 
 		var pkg = require(files.npm);
 
 		gulp.src(files.toClean)
-			.pipe(clean(), {read: false})
+			.pipe(clean(), {read: false, force: true})
 
 		gulp.src(files.jsSrc)
 			.pipe( uglify().on('error', gutil.log) )
 			.pipe( gulp.dest(files.jsDstPath) );
+
+		gulp.src(files.htmlSrc)
+			.pipe( srcMaps.init() )
+			.pipe( wiredep({
+				directory: files.vendor,
+				bowerJson: require(files.bower),
+			}) )
+			.pipe( srcMaps.write() )
+			.pipe( useref() )
+			.pipe( gulpif('*.css', minCss()) )
+			.pipe( gulpif('*.js', uglify()) )
+			.pipe( gulp.dest(files.htmlDstPath) );
 
 		// funnily prints the copyright twice!
 		gulp.src(files.jsDst)
 			.pipe( header(fs.readFileSync(files.copyright, 'utf8'), {version: fs.readFileSync(files.version)}) )
 			.pipe( gulp.dest(files.jsDstPath) );
 
-		gulp.src(files.cssSrc, { read: false })
-			.pipe( inject(gulp.src(files.cssSrc, {read: false})) )
-			.pipe( gulp.dest(files.cssDstPath) );
-
-		gulp.src(files.htmlSrc)
-			.pipe(wiredep({
-				directory: files.vendor,
-				bowerJson: require(files.bower),
-			}))
-			.pipe( gulp.dest(files.htmlDstPath) );
 	});
 	/*
 		gulp.src('src/index.html')
--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,29 @@
 /* app logic, injected into footer part */
 
+function mk_love_post() {
+
+  "use strict";
+
+  $('body').on('click', '.mk-love-this', function () {
+    var $this = $(this),
+      $id = $this.attr('id');
+
+    if ($this.hasClass('item-loved')) return false;
+
+    if ($this.hasClass('item-inactive')) return false;
+
+    var $sentdata = {
+      action: 'mk_love_post',
+      post_id: $id
+    }
+
+    $.post(ajaxurl, $sentdata, function (data) {
+      $this.find('.mk-love-count').html(data);
+      $this.addClass('item-loved');
+    });
+
+    $this.addClass('item-inactive');
+    return false;
+  });
+
+}
